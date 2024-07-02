af3693dea03a35de70680f537693110a951a97f6
vvquy01 <vivanquy20804@gmail.com>
2024-07-03 01:05:51 +0700

    Khoi tao

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Thiệp của chúng tớ",
+    introTitle: 'Hiii Xuân Mai!',
+    introDesc: `Chúng tớ rất vui vì được gửi lá thư này đến cậu
+    hãy dành một chút thời gian theo chân chúng tớ nhé `,
+    btnIntro: 'Ấn zô đây nè ><',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Đồng ý <33',
+    btnNo: 'Không nha :3',
+    question: 'Xác nhận lại lần nữa để chúng tớ bíc cậu hông bị ép nka<3',
+    btnReply: 'Gửi cho chúng tớ <3',
+    reply: 'Oke nhaaaa <33333333',
+    mess: 'Mình biết mà hehe🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Mình háo hức gặp cậu quá trời! Chờ mình sắp xếp lịch rồi thông báo tới cậu ngày tháng nhen :3',
+    btnAccept: 'Okiiiii lun <3',
+}
